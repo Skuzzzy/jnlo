359e523eb98403db0c15258403cd4d8f9f9555b1
Daniel Baird <JamesDanielBaird@gmail.com>
2015-02-05 15:22:37 -0700

    Cleaning up main

--- src/Main.java
+++ src/Main.java
@@ -8,38 +8,10 @@ public class Main
     public static void main(String[] args)
     {
         String expr = "c*!(a+b)";
-        ArrayList<LogToken> chips = Parser.parseString(expr);
-
         System.out.println(expr);
-
-        for(LogToken chip : chips)
-        {
-            //System.out.println(chip.getTokenType());
-        }
-
-        ArrayList<LogToken> dips = Parser.convertToPostfix(chips);
-
-        /*
-        for(LogToken chip : dips)
-        {
-            System.out.print(chip.toString()+" ");
-        }
-        System.out.println("");
-        */
-
-        ;
-        ArrayList<String> asd = Evaluator.getLexographicalOrderOfVariables(chips);
-        /*
-        for(String s : asd )
-        {
-            System.out.println(s);
-        }
-        */
-
-        boolean[] inputs = {true,true,true};
-        //System.out.println("a = true    b = true  c = true");
-        //System.out.println(Evaluator.evaluatePostfixExpression(dips,inputs));
-        Evaluator.displayTruthTable(dips);
+        ArrayList<LogToken> tokenExpression = Parser.parseString(expr);
+        ArrayList<LogToken> postfixExpression = Parser.convertToPostfix(tokenExpression);
+        Evaluator.displayTruthTable(postfixExpression);
     }
 
 }
